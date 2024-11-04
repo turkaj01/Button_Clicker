7a16f419aa32182f72d28beb6eedcee2d758e9dd
Anton Turkaj <antonturkaj@hotmail.co.uk>
2024-11-04 15:11:19 +0100

    Button Clicker Assignment

---
+++ script.js
@@ -0,0 +1,9 @@
+function login(element){
+    element.innerText = "Logout"
+}
+function removeDefinition(element) {
+    element.remove()
+}
+function like(element){
+    alert("Ninja was Liked")
+}
